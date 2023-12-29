66bf0526f3e6eeaf1c8051dc307e21eca0a3f2de
alisha raghav <alisharaghav09@gmail.com>
2023-12-29 23:23:34 +0530

    added views

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from models import link
 
 # Create your views here.
+def scrape(request):
+    page = requests.get('https://www.facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+  
+
+    for link in soup.find_all('a'):
+       link_address = link.get('href')
+       link_text = link.string
+
+    return render(request,'result.html', {'link_address':link_address})   
